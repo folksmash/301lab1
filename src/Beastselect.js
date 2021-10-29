a815edf4d2aef5d3efb2bb801c5f064ed1efed26
Jordan Folk <jordan.folk7@gmail.com>
2021-10-28 17:43:20 -0700

    added functional modal to app

--- src/Beastselect.js
+++ src/Beastselect.js
@@ -9,21 +9,22 @@ import {Modal, Button, Card} from "react-bootstrap/";
 class Beastselect extends React.Component {
 
   render() {
+    console.log(this.props.featuredBeast)
     return (
       <div>
 
       <Modal  show={this.props.show} onHide={this.props.handleClose}>
         <Modal.Header>
-          {/* <Modal.Title>{this.props.amimal.title}</Modal.Title> */}
+          <Modal.Title>{this.props.featuredBeast.title}</Modal.Title>
           <Card.Img
             variant="top"
-            // src={this.props.amimal.image_url}
+            src={this.props.featuredBeast.image_url}
             alt="horned"
             title="horned"
           />
         </Modal.Header>
         <Modal.Body>
-            {/* <Card.Text>{this.props.amimal.description}</Card.Text> */}
+            <Card.Text>{this.props.featuredBeast.description}</Card.Text>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={this.props.handleClose}>
